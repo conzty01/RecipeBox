2d944fc26db96fe23b5292cda902e9145cb79254
Tyler <conzty01@luther.edu>
2020-01-02 23:12:13 -0600

    Partially implemented passport-local

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* POST login function */
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log(req.body);
+    res.redirect
+  }
+);
+
 module.exports = router;
